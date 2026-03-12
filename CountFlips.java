0f9d2ab7528f1eff8a24d32033310bf9c163aeac
AmaBurg <amaburges@hartford.edu>
2026-03-12 14:25:04 -0400

    Completed CountFlips program

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,19 @@
 public class CountFlips {
   public static void main(String[] args) {
+    int Heads = 0;
+    int Tails = 0;
+    Coin coinFlips = new Coin();
+
+    for (int i = 0; i < 100; i++) {
+      coinFlips.flip();
+      if (coinFlips.isHeads()) {
+        Heads++;
+      } else {
+        Tails++;
+      }
+    }
+
+    System.out.println("Heads: " + Heads);
+    System.out.println("Tails: " + Tails);
   }
 }
